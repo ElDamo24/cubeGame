a8d2542149aaa677bba424694feb71f5e67478aa
Damian <damianlobos24@gmail.com>
2020-09-25 17:57:22 -0300

    firstCommit
    Uploading the cube game.

---
+++ Learning about games/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Permissions;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce;
+    public float leftRightForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        forwardForce =3000f;
+        leftRightForce = 50f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(leftRightForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-leftRightForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ Learning about games/Assets/Scripts/playerColision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class playerColision : MonoBehaviour
+{
+    public Player movement;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
